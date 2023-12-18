3e24aaebf6c5505d31a54dd9bf70c0b05f8023e2
namekuji <okosimasu3@gmail.com>
2023-12-18 21:52:21 +0900

    204

--- preload.js
+++ preload.js
@@ -7,14 +7,11 @@ ipcRenderer.on('ESC', () => {
 
 function cssInject() {
     let cssIn = `<link rel="stylesheet" href="slug://${path.join(__dirname, 'impact.css')}">`
-    document.head.insertAdjacentHTML('beforeend', cssIn);
+    document.body.insertAdjacentHTML('afterbegin', cssIn);
 }
-function title() {
-    document.getElementsByTagName(title)[0].innerText = "いんぱくとの煮込みカレー"
-}
+
 document.addEventListener('DOMContentLoaded', () => {
     cssInject()
-    title()
 })
 window.closeClient = () => {
     ipcRenderer.send('exitClient');
@@ -25,29 +22,6 @@ function onReturnHolderGenerated() {
     console.log("#returnHolderが生成されました!");
     document.getElementById("returnHolder").setAttribute("onclick", "window.location.href='https://krunker.io/'")
 }
-
-//Compの画面にModを追加
-function compModGen() {
-    let dom = `<div class="compMenBtnS" onmouseenter="SOUND.play(&quot;tick_0&quot;,.1)" onclick="playSelect(),showWindow(4)" style="background-color:#f5479b">
-    <span class="material-icons" style="color:#fff;font-size:40px;vertical-align:middle;margin-bottom:12px">color_lens</span>
-    </div>`
-    let dom2 = `<div class="compMenBtnS" onmouseenter="SOUND.play(&quot;tick_0&quot;,.1)" onclick="playSelect(),showWindow(42)" style="background-color:#b447ff">
-    <span class="material-icons" style="color:#fff;font-size:40px;vertical-align:middle;margin-bottom:12px">add_circle</span>
-    </div>`
-    document.getElementById('compBtnLst').insertAdjacentHTML('beforeend', dom);
-    document.getElementById('compBtnLst').insertAdjacentHTML('afterBegin', dom2);
-
-}
-function checkElementExistence() {
-    var element = document.getElementById('compBtnLst');
-    if (element) {
-        compModGen()
-    }
-}
-
-
-// ページが読み込まれた時にcheckElementExistence関数を実行する
-window.onload = checkElementExistence;
 
 // DOMContentLoadedイベントが発生したときに実行されるコード
 document.addEventListener("DOMContentLoaded", function () {
