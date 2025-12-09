266ec30820bb7bdd609ae2260062db0770742134
NamekujiLSDs <okosimasu3@gmail.com>
2025-12-09 22:31:41 +0900

    dev mode off

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const config = new store({
 app.userAgentFallback = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
 
 //DevMode
-autoUpdater.forceDevUpdateConfig = true;
+// autoUpdater.forceDevUpdateConfig = true;
 
 
 //脆弱性の対策らしい？知らんけど
